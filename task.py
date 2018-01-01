5e0a2cec162669fe028f2e5c8055a83e956057a2
neex-io <neex.io@protonmail.com>
2017-12-31 19:25:08 -0800

    Add Task class basics

--- task.py
+++ task.py
@@ -10,4 +10,38 @@ class Task:
     notes = None
     time = None
 
-    pass
+    def __init__(self, name, time, notes, date):
+        """[summary]
+        
+        Arguments:
+            name {[type]} -- [description]
+            time {[type]} -- [description]
+            notes {[type]} -- [description]
+            date {[type]} -- [description]
+        """
+        self.name = name
+        self.date = date
+        self.notes = notes
+        self.time = time
+
+    def __repr__(self):
+        representation = """-------------------
+Name       - {}
+Time (min) - {}
+Notes      - {}
+Date       - {}
+-------------------\n""".format(self.name, 
+                                self.time, 
+                                self.notes, 
+                                self.date)
+        return representation
+
+    def keep_string(self):
+        cvs_row = "{}, {}, {}, {}".format(self.name,
+                                          self.time,
+                                          self.notes,
+                                          self.date) 
+        return cvs_row
+
+n = Task(1,2,3,4)
+print(n)
