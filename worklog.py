dd7138747526fa04706ca9d754c79f8f2981c9f4
Unknown <neex.io@protonmail.com>
2018-01-01 19:47:19 -0800

    Ask for task data from stdin

--- worklog.py
+++ worklog.py
@@ -14,12 +14,43 @@ def display_menu():
     
 
 def get_new_task():
-    """[summary]
-    
+    """Allows user to enter a task name, time spent, and optional notes.
+
     Returns:
         [type] -- [description]
     """
-    pass
+    name = None
+    date = None
+    notes = None
+    time = None
+
+    # get Task name and time
+    while not name or name.isspace():
+        # is none or white space (ENTER is a white space too)
+        name = input('Enter task name or press (q) to quit.\n>>>')
+        if name.lower() == 'q':
+            sys.exit(0)
+    while not time:
+        try:
+            time = int(input('Enter time spent on task (in min).\n>>>'))
+        except ValueError:
+            print("Please only enter an integer.")
+
+    # get task notes
+    notes = input(
+        "Enter notes about the task (optional). Pres ENTER to skip.\n>>>")
+    if notes == "":
+        notes = None
+
+    # set task entry date
+    # format: '2018/12/31'
+    date = datetime.datetime.now().strftime('%Y/%m/%d')
+
+    print(name, time, notes, date)
+    return (name, time, notes, date)
+
+
+get_new_task()
 
 def save_to_file():
     """[summary]
